d7435745377519872bdb8807b8b2d05aa5ca1e90
Prabhash Rai <raiprabh@groww.in>
2023-01-24 17:06:08 +0530

    form complete

--- checking.js
+++ checking.js
@@ -1,51 +1,8 @@
-const save_button = document.getElementById("save");
-save_button.addEventListener('click', check_filled_data);
-
-const reset_button = document.getElementById("reset");
-reset_button.addEventListener('click', remove_filled_details);
-
 const spouse_name = document.getElementById("name_of_spouse");
+const terms_conditions = document.getElementById("terms_conditions");
 
 function marital() {
     spouse_name.disabled = true;
-}
-
-function remove_filled_details() {
-    const table = document.getElementById("input_form").getElementsByTagName("tr");
-
-    const total_fields = table.length;
-
-    for (let i = 0; i < total_fields; i++) {
-        const row_data = table[i].getElementsByTagName("td");
-
-        for (let j = 0; j < row_data.length; j++) {
-            const data_filled = row_data[j].getElementsByTagName("input");
-
-            if (data_filled.length === 0) continue;
-
-            for (let input = 0; input < data_filled.length; input++) {
-
-                //check text inputs
-                if (data_filled[input].type === "text" && data_filled[input].value) {
-                    data_filled[input].value = "";
-                }
-
-                // check radio inputs
-                if (data_filled[input].type === 'radio') {
-
-                    const options = document.getElementsByName(data_filled[input].name);
-
-                    for (const option of options) {
-                        if (option.checked) {
-                            option.checked = false;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    spouse_name.disabled = false;
 }
 
 function check_filled_data() {
@@ -92,7 +49,7 @@ function check_filled_data() {
     if (spouse_name.disabled) req_fields.delete("name of spouse");
 
     if (req_fields.size) {
-        alert("Please fill these details => \n" + [...req_fields]);
+        alert("Please fill " + [...req_fields]);
 
         for (element of req_fields) {
             const empty_elements =  document.getElementsByName(element);
@@ -112,7 +69,7 @@ function check_filled_data() {
             alert("Thanks for filling the form!!!");
         }
         else {
-            const terms_conditions = document.getElementById("terms_conditions");
+            terms_conditions.focus();
             alert("Please accept the terms and conditions.");
         }
     }
